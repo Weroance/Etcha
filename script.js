c377791baec89d07c76b3f6d90928b2894f702ad
Weroance <danielrjansen@protonmail.com>
2024-05-09 15:55:43 -0400

    Add: variables and pseudocode

---
+++ script.js
@@ -0,0 +1,10 @@
+generate 16 x 16 grid of white background divs "pixels" 
+    append with flex distribution
+listen for hover over all divs (for each do?)  ????
+    upon event change "pixel" divs black
+listen for button to create new grid
+    event: prompt for single grid dimension default to 16
+
+// EXTRA credit: listen for button
+//                  hover causes random color or "pixel" has random color assignment
+// EXTRA credit II listen for button use %10 shading on each hover event   
